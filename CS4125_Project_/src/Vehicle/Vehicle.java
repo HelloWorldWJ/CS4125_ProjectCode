6bc46fcd49138f2ce41fa476f3c3378a79c4ecfd
HelloWorldWJ <43991412+HelloWorldWJ@users.noreply.github.com>
2021-11-18 13:57:17 +0000

    1118 1356
    vehicle more

--- CS4125_Project_/src/Vehicle/Vehicle.java
+++ CS4125_Project_/src/Vehicle/Vehicle.java
@@ -1,17 +1,95 @@
 package Vehicle;
 
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+import Map.Lane;
+import Map.Road;
 import SetElement.SetElement;
 
 public class Vehicle {
 	
-	Enum Brand;
-	double length;
-	double width;
-	double speed;
-	int current_location_x;
-	int current_location_y;
+	public enum Brand{
+		Benz,Toyota,Nissan,Audi,Ford
+	}
+	private double vehicleLength;
+	private double vehicleWidth;
+	private Lane track;
+	private Road road;
+	private double speed, angle;
+//	private double maxSpeed, currentSpeed, angle;
+	private Point position;
+	private int current_location_x;
+	private int current_location_y;
+	private String imagePath;
+	
+//	private int currentCell, vehicleWidth, vehicleHeight, currentLaneId, vehicleId;
+//	private double maxSpeed, currentSpeed, angle;
+//	private Lane track;
+//	private Road road;
+//	private Point position;
+//	private Color color;
+//	private String imagePath;
+//	private I_VehicleState state;
+//	private BufferedImage carImage;
+	
+	
+	public Vehicle() {
+		
+	}
+	
 	
 //	void Set_attribute();
 //	void Update_attribute(v:Vehicle);
+	public Vehicle(double length, double width, double speed, double angle, Point xy, Road road, Lane track, String imagePath) {
+//		this.Brand = Brand;
+		this.vehicleLength = length;
+		this.vehicleWidth = width;
+//		this.vehicleWidth = getVehicleWidthRelevantToLaneNum(numLanes);
+//		this.vehicleHeight = getVehicleHeightRelevantToLaneNum(numLanes);
+		this.speed = speed;
+		this.angle = angle;
+		this.position = xy;
+		this.road = road;
+		this.track = track;
+//		this.track = road.getLane(currentLaneId);
+		this.imagePath = imagePath;
+//		this.current_location_x = current_location_x;
+//		this.current_location_y = current_location_y;
+	}
+	
+	public Point getPosition(){
+		return position;
+	}
+	
+	public double getAngle(){
+		return angle;
+	}
+	
+	public Road getRoad(){
+		return road;
+	}
 
+	public Lane getLane() {
+		return track;
+	}
+	
+	public String getVehicleImagePath() {
+		return this.imagePath;
+	}
+	
+	public double getVehicleHeight() {
+		return vehicleLength;
+	}
+	
+	public double getVehicleWidth() {
+		return vehicleWidth;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	
 }
--- CS4125_Project_/src/Vehicle/VehicleFactory.java
+++ CS4125_Project_/src/Vehicle/VehicleFactory.java
@@ -1,12 +1,21 @@
 package Vehicle;
 
+import java.awt.Point;
+
+import Map.Lane;
+import Map.Road;
+
 public class VehicleFactory {
-	public static enum VehicleType
-	{
-		Fastcar,
-		Slowcar,
-		Mediumcar
-	};
-	//public  
+	
+//	public static enum VehicleType
+//	{
+//		Fastcar,
+//		Slowcar,
+//		Mediumcar
+//	};
+	
+	public Vehicle createVehicle(double length, double width, double speed, double angle, Point xy, Road road, Lane track, String imagePath) {
+		return new Vehicle(length, width, speed, angle, xy, road, track, imagePath);
+	}
 
 }
