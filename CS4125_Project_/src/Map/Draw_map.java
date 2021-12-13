0b2fef8c350e6fb5f82111011f7c731c13f40b41
ul20125313 <72030425+ul20125313@users.noreply.github.com>
2021-12-13 16:15:45 +0000

    12/13

--- CS4125_Project_/src/Map/Draw_map.java
+++ CS4125_Project_/src/Map/Draw_map.java
@@ -1,6 +1,7 @@
 package Map;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.geom.AffineTransform;
@@ -8,24 +9,23 @@ import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
-
 import Driver.Driver;
 import Driver.IrritableDriver;
 import Driver.NormalDriver;
-import Driver.DriverFactory;
-import Vehicle.Ferrari;
+import Observer.Speed_Monitor;
+import Observer.Track1_Ob;
+import Observer.Track2_Ob;
+import Observer.Track3_Ob;
+import Observer.Track4_Ob;
 import Vehicle.Vehicle;
-import Vehicle.VehicleTypeFactory;
-import Driver.DriverFactory.DriverTemper;
 
 
-import java.awt.BasicStroke;
+
+
 
 public class Draw_map implements I_Draw_map{
-	private int index;
 	private int Map_size_x;
 	private int Map_size_y;
 	private IDisplay display;
@@ -35,10 +35,14 @@ public class Draw_map implements I_Draw_map{
 	private final String BACKGROUND_PATH;
 	private Point center;
 	private ArrayList<Driver>drivers;
-	private Vehicle vehicle;
-	private Lane1 lane1;
-	private Lane2 lane2;
+
 	private ArrayList<Lane>lanes;
+	private Speed_Monitor m ;
+	private Track1_Ob t1; 
+	private Track2_Ob t2; 
+	private Track3_Ob t3; 
+	private Track4_Ob t4; 
+	
 	
 	
 	public Draw_map(IDisplay idis, double map_wi, double map_he, ArrayList<Driver>drivers, ArrayList<Lane> lanes)
@@ -46,28 +50,34 @@ public class Draw_map implements I_Draw_map{
 		this.Map_size_x = (int)map_wi;
 		this.Map_size_y = (int)map_he;   			    
 	    this.center = new Point((Map_size_x), (Map_size_y));
-		int currentX = (int) center.getX();
-        int currentY = (int) center.getY();
-		System.out.println("Map size: " + Map_size_x + " " + Map_size_y);
-		System.out.println("----------------------------");
-		System.out.println("(Vehicle--DriverTemper--Speed)");
-//        System.out.println("Draw_map class ScreenX: "+currentX+" ScreenY "+currentY);
-        
-		
         this.lanes = lanes;   
 	    this.drivers = drivers;		    
 		this.display = idis;	
 		this.BACKGROUND_PATH = "background.png";
 		this.background =  create_background();
-		this.lane1 = new Lane1();
-		this.lane2 = new Lane2();
 		this.set_map();
+		print_important_info();
+		Observer_init();
 	
 	}
 	
-
-
-
+	private void print_important_info()
+	{
+		System.out.println("Map size: " + Map_size_x + " " + Map_size_y);
+		System.out.println("----------------------------");
+		System.out.println("(Vehicle--DriverTemper--Speed)");	
+	}
+	
+	private void Observer_init()
+	{
+		this.m = new Speed_Monitor();
+		this.t1 = new Track1_Ob(m);
+		this.t2 = new Track2_Ob(m);
+		this.t3 = new Track3_Ob(m);
+		this.t4 = new Track4_Ob(m);
+		
+	}
+	
 	public void set_map() //create the graphics of map
 	{
 		
@@ -91,10 +101,18 @@ public class Draw_map implements I_Draw_map{
 
         this.graphics.drawImage(vehicle.getCarImage(), at, null);
 	}
-	private void drawSpeed()
+	
+	private void draw_updateinfo_in_all_tracks(double speed, String drivername, int height)
 	{
-		
+
+		String speed_str = "" + speed;
+		//System.out.println(speed_str);
+		this.graphics.setColor(Color.RED);
+		this.graphics.setFont(new Font("Calibri", Font.BOLD, 13));
+		this.graphics.drawString(drivername + "  " + speed_str +" km/h", 0, height);
+	
 	}
+	
 	
 	private void drawLane(Lane lane)
 	{
@@ -111,8 +129,11 @@ public class Draw_map implements I_Draw_map{
 	}
 	
 	
-	public void render() {
-		
+	public void render() 
+	{
+
+//		Speed_Monitor m = new Speed_Monitor();
+//		Speed_show s1 = new Speed_show(m); 
 		this.drawbackground();
 		for (Lane l : this.lanes) 
 		{
@@ -120,30 +141,47 @@ public class Draw_map implements I_Draw_map{
         }
 		for (Driver driver : this.drivers)
 		{
-			this.drawVehicle(driver.getVehilce());
-			//System.out.println(driver.getClass().toString());
 			
+			this.drawVehicle(driver.getVehilce());			
 			if(driver.getClass().toString().equals("class Driver.NormalDriver")) {
-				//System.out.println("1");
 				NormalDriver NormalDriver = (NormalDriver)driver;
 				NormalDriver.Drive();
-				NormalDriver.pri();
+				//NormalDriver.pri();
 			}
-			else if(driver.getClass().toString().equals("class Driver.IrritableDriver")) {
-				//System.out.println("2");
+			else if(driver.getClass().toString().equals("class Driver.IrritableDriver")) 
+			{
 				IrritableDriver IrritableDriver = (IrritableDriver)driver;
 				IrritableDriver.Drive();
-				IrritableDriver.pri();
 			}
-			//driver.limitSpeed();
-			//driver.pri();
-		}
-        //this.drawVehicle(this.driver.getVehilce());
+
+		}	
+		
+		Observe_Distribute(drivers);
 		this.buffer.show();
+    }
+	
+	private void Observe_Distribute(ArrayList<Driver>ds)
+	{
+		ArrayList<Double>speeds = new ArrayList<>();
+		speeds.add(ds.get(0).getVehilce().getSpeed());
+		speeds.add(ds.get(1).getVehilce().getSpeed());
+		speeds.add(ds.get(2).getVehilce().getSpeed());
+		speeds.add(ds.get(3).getVehilce().getSpeed());
+		
+		ArrayList<String>driverNames = new ArrayList<>();
+		driverNames.add(ds.get(0).getName());
+		driverNames.add(ds.get(1).getName());
+		driverNames.add(ds.get(2).getName());
+		driverNames.add(ds.get(3).getName());
+		
+		
+		this.m.set_info(driverNames,speeds);	
+        this.draw_updateinfo_in_all_tracks(this.t1.Track1_speed(), this.t1.Track1_drivernames(), 510);
+        this.draw_updateinfo_in_all_tracks(this.t2.Track2_speed(), this.t2.Track2_drivernames(), 530);
+        this.draw_updateinfo_in_all_tracks(this.t3.Track3_speed(), this.t3.Track3_drivernames(), 550);
+        this.draw_updateinfo_in_all_tracks(this.t4.Track4_speed(), this.t4.Track4_drivernames(), 570);
 	}
-	
-	
-	
+
 	
 	private BufferedImage create_background()
 	{
@@ -153,8 +191,7 @@ public class Draw_map implements I_Draw_map{
 			return ImageIO.read(new File(this.BACKGROUND_PATH));	
 			            
 		} 
-		
-		
+
 		catch (Exception e) 
 		{	
 			System.out.print(e);
